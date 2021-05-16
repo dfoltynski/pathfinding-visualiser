6bb17f433ed850918c58b76aabfb71bac14783a5
dfoltynski <dawidfoltynski7@gmail.com>
2021-05-16 18:57:52 +0200

    styling legend

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -5,9 +5,23 @@ import "../App.css";
 export default function Legend() {
   return (
     <ul className="legend">
-      <li className="legend__item">Start node</li>
-      <li className="legend__item">End node</li>
-      <li className="legend__item">Barrier node</li>
+      <li className="legend__item">
+        <div className="legend__node" style={{ backgroundColor: "blue" }}></div>{" "}
+        Start node
+      </li>
+      <li className="legend__item">
+        {" "}
+        <div
+          className="legend__node"
+          style={{ backgroundColor: "yellow" }}
+        ></div>
+        End node
+      </li>
+      <li className="legend__item">
+        {" "}
+        <div className="legend__node" style={{ backgroundColor: "pink" }}></div>
+        Barrier node
+      </li>
     </ul>
   );
 }
