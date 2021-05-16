2e1a3aacadf9781c2c3fa3c38dbee20cc439d685
dfoltynski <dawidfoltynski7@gmail.com>
2021-05-16 19:55:39 +0200

    generate random start and end node

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,20 +1,44 @@
 import React, { useEffect, useRef } from "react";
 
 export default function Grid() {
-  const table = useRef<HTMLTableElement>(document.createElement("table"));
+  const tableRef = useRef<HTMLTableElement>(document.createElement("table"));
+
+  const handleNodeClick = (event: MouseEvent) => {
+    event.preventDefault();
+    console.log(event.currentTarget);
+  };
+
+  const generateRandomCoords = (): string => {
+    const randomCoords: string = `${Math.floor(
+      Math.random() * 31
+    )}:${Math.floor(Math.random() * 21)}`;
+    return randomCoords;
+  };
 
   useEffect(() => {
-    for (let x = 0; x < 20; x++) {
-      let row = document.createElement("tr");
+    const startNodeCoords = generateRandomCoords();
+    const endNodeCoords = generateRandomCoords();
+
+    for (let y = 1; y < 21; y++) {
+      let row: HTMLTableRowElement = document.createElement("tr");
       row.classList.add("gird__row");
-      for (let y = 0; y < 36; y++) {
-        let node = document.createElement("td");
+      for (let x = 1; x < 37; x++) {
+        let node: HTMLTableDataCellElement = document.createElement("td");
         node.classList.add("grid__node");
+        node.id = `${x}:${y}`;
+        node.onclick = handleNodeClick;
         row.appendChild(node);
       }
-      table.current.appendChild(row);
+      tableRef.current.appendChild(row);
+    }
+
+    const startNode = document.getElementById(startNodeCoords);
+    const endNode = document.getElementById(endNodeCoords);
+    if (startNode && endNode) {
+      startNode.classList.add("start");
+      endNode.classList.add("end");
     }
   }, []);
 
-  return <table className="grid" ref={table}></table>;
+  return <table className="grid" ref={tableRef}></table>;
 }
