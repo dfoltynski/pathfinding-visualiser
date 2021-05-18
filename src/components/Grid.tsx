43fe3ce5eb9a9734c27576d180b6fff97bd0902c
dfoltynski <dawidfoltynski7@gmail.com>
2021-05-18 20:32:14 +0200

    basic node dragging

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,11 +1,48 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Grid() {
+  let mouseDown: boolean = false;
+  let nodeFlag: string = "";
+
   const tableRef = useRef<HTMLTableElement>(document.createElement("table"));
 
   const handleNodeClick = (event: MouseEvent) => {
     event.preventDefault();
     console.log(event.currentTarget);
+  };
+
+  const handleDrag = (ev: any) => {
+    if (mouseDown) {
+      let prevNode = document.querySelectorAll(`.${nodeFlag}`)[1];
+      prevNode?.classList.remove(`${nodeFlag}`);
+      console.log(ev.fromElement);
+      ev.fromElement.classList.add(`${nodeFlag}`);
+    }
+  };
+
+  const createNode = (x: number, y: number): HTMLTableDataCellElement => {
+    const node: HTMLTableDataCellElement = document.createElement("td");
+    node.classList.add("grid__node");
+    node.id = `${x}:${y}`;
+
+    node.addEventListener("mouseover", handleDrag);
+
+    node.addEventListener("mousedown", (ev: any) => {
+      console.log("draging");
+      if (ev.path[0].classList[1] == "start") {
+        mouseDown = true;
+        nodeFlag = "start";
+      } else if (ev.path[0].classList[1] == "end") {
+        mouseDown = true;
+        nodeFlag = "end";
+      }
+    });
+
+    node.addEventListener("mouseup", () => {
+      mouseDown = false;
+    });
+
+    return node;
   };
 
   const generateRandomCoords = (): string => {
@@ -23,10 +60,7 @@ export default function Grid() {
       let row: HTMLTableRowElement = document.createElement("tr");
       row.classList.add("gird__row");
       for (let x = 1; x < 37; x++) {
-        let node: HTMLTableDataCellElement = document.createElement("td");
-        node.classList.add("grid__node");
-        node.id = `${x}:${y}`;
-        node.onclick = handleNodeClick;
+        let node: HTMLTableDataCellElement = createNode(x, y);
         row.appendChild(node);
       }
       tableRef.current.appendChild(row);
--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -6,20 +6,16 @@ export default function Legend() {
   return (
     <ul className="legend">
       <li className="legend__item">
-        <div className="legend__node" style={{ backgroundColor: "blue" }}></div>{" "}
-        Start node
+        <div className="legend__node start"></div> Start node
       </li>
       <li className="legend__item">
         {" "}
-        <div
-          className="legend__node"
-          style={{ backgroundColor: "yellow" }}
-        ></div>
+        <div className="legend__node end"></div>
         End node
       </li>
       <li className="legend__item">
         {" "}
-        <div className="legend__node" style={{ backgroundColor: "pink" }}></div>
+        <div className="legend__node barrier"></div>
         Barrier node
       </li>
     </ul>
